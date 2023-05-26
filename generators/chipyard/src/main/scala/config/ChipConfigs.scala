9636ab0e0b4b76652f114dbbcb2d319ddadf0ac0
Allison Husain <allison.husain@berkeley.edu>
2023-05-25 20:01:58 -0700

    Base digital config

--- generators/chipyard/src/main/scala/config/ChipConfigs.scala
+++ generators/chipyard/src/main/scala/config/ChipConfigs.scala
@@ -4,7 +4,7 @@ import org.chipsalliance.cde.config.{Config}
 import freechips.rocketchip.diplomacy._
 
 // A simple config demonstrating how to set up a basic chip in Chipyard
-class ChipLikeQuadRocketConfig extends Config(
+class SRAMDigitalConfig extends Config(
   //==================================
   // Set up TestHarness
   //==================================
@@ -14,16 +14,43 @@ class ChipLikeQuadRocketConfig extends Config(
   //==================================
   // Set up tiles
   //==================================
-  new freechips.rocketchip.subsystem.WithAsynchronousRocketTiles(3, 3) ++    // Add rational crossings between RocketTile and uncore
-  new freechips.rocketchip.subsystem.WithNBigCores(4) ++                     // quad-core (4 RocketTiles)
+  // new chipyard.config.WithTLSerialLocation(
+  //   freechips.rocketchip.subsystem.FBUS,
+  //   freechips.rocketchip.subsystem.PBUS) ++                       // attach TL serial adapter to f/p busses
+  // new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++ // use incoherent bus topology
+  // new freechips.rocketchip.subsystem.WithNMemoryChannels(0) ++
+  new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
+
+  // new freechips.rocketchip.subsystem.With1TinyCore(scratchpadBase = None) ++             // single tiny rocket-core
+  // new freechips.rocketchip.subsystem.With1TinyCore(scratchpadBase = Some(0x11000000)) ++             // single tiny rocket-core
+  // new freechips.rocketchip.subsystem.With1TinyCore(scratchpadBase = Some(0x08000000)) ++             // single tiny rocket-core
+  new freechips.rocketchip.subsystem.WithL1DCacheSets(sets = 64) ++ // 64 sets, 4K cache
+  new freechips.rocketchip.subsystem.With1TinyCore(scratchpadBase = None) ++             // single tiny rocket-core
+
+  new chipyard.config.WithBroadcastManager ++ // Replace L2 with a broadcast hub for coherence
+  new freechips.rocketchip.subsystem.WithBufferlessBroadcastHub() ++ // Remove buffers from broadcast manager
+
+  new testchipip.WithBackingScratchpad(base = 0x08000000, mask = ((4<<10)-1)) ++ // 4 KB
 
   //==================================
   // Set up I/O
   //==================================
-  new testchipip.WithSerialTLWidth(4) ++
-  new chipyard.harness.WithSimAXIMemOverSerialTL ++                                     // Attach fast SimDRAM to TestHarness
+  /* 1GB TSI region, 1ch */
+  new testchipip.WithSerialTLWidth(1) ++
   new chipyard.config.WithSerialTLBackingMemory ++                                      // Backing memory is over serial TL protocol
-  new freechips.rocketchip.subsystem.WithExtMemSize((1 << 30) * 4L) ++                  // 4GB max external memory
+  new freechips.rocketchip.subsystem.WithExtMemSize((1 << 30) * 1L) ++                  // 1GB max external memory
+
+  /* 1x UART */
+  new chipyard.config.WithUARTOverride(address = 0x10020000, baudrate = 115200) ++
+  
+  /* XIP SPI with 194's PSRAM changes */
+  new chipyard.harness.WithSimSPIFlashModel(true) ++       // add the SPI flash model in the harness (writeable)  
+  new chipyard.config.WithSPIFlash(address = 0x10021000, fAddress = 0x20000000, size = 0x10000000) ++             // add the SPI psram controller (1 MiB)
+
+  /* boot_sel=0: hang/wait for TSI. boot_sel=1: self-boot (priority bootloader, try SPI first then BEBE) */
+  new testchipip.WithCustomBootPinAltAddr(address = 0x000100c0) ++
+
+  /* JTAG is in the default config */
 
   //==================================
   // Set up clock./reset
@@ -38,7 +65,6 @@ class ChipLikeQuadRocketConfig extends Config(
   new chipyard.config.WithFbusToSbusCrossingType(AsynchronousCrossing()) ++  // Add Async crossing between SBUS and FBUS
   new chipyard.config.WithCbusToPbusCrossingType(AsynchronousCrossing()) ++  // Add Async crossing between PBUS and CBUS
   new chipyard.config.WithSbusToMbusCrossingType(AsynchronousCrossing()) ++  // Add Async crossings between backside of L2 and MBUS
-  new testchipip.WithAsynchronousSerialSlaveCrossing ++                      // Add Async crossing between serial and MBUS. Its master-side is tied to the FBUS
 
   new chipyard.config.AbstractConfig)
 
