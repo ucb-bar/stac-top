d6c7c787f883e46c6e805070ca355e4a92520f9b
Viansa Schmulbach <ansa@bwrcrdsl-2.eecs.berkeley.edu>
2023-05-27 13:12:54 -0700

    add generator

---
+++ generators/sram-test-unit/src/main/scala/Configs.scala
@@ -0,0 +1,19 @@
+package sramtestunit
+
+case class ProgrammableBistParams(
+  addr: AddressSet,
+  elementTableLength : Int = 8,
+  operationsPerElement : Int = 8,
+  patternTableLength : Int = 8,
+)
+
+
+// case object ProgrammableBistKey extends Field[Option[ProgrammableBistParams]](None)
+
+//trait CanHavePeripheryProgrammableBist {
+  // connect Programmable BIST to subsystem here
+//}
+
+// class WithProgrammableBist() extends Config((site, here, up) => {
+//  case ProgrammableBistKey => Some(ProgrammableBistParams())
+//})
