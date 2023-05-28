beeb85dde22a4ebb76799bf0ab35739b39ecfc37
Rahul Kumar <rahulkumar@berkeley.edu>
2023-05-28 09:45:50 -0700

    Add MISR test with varying input

--- generators/sram-bist/src/test/scala/misr/MISRSpec.scala
+++ generators/sram-bist/src/test/scala/misr/MISRSpec.scala
@@ -51,5 +51,35 @@ class MISRSpec extends AnyFlatSpec with ChiselScalatestTester {
       c.io.out.expect(Vec.Lit(false.B, false.B, false.B, false.B))
     }
   }
+
+  it should "work with changing input" in {
+    test(new MaxPeriodFibonacciMISR(4)) { c => 
+      c.io.in.poke(Vec.Lit(true.B, false.B, true.B, false.B))
+      c.io.out.expect(Vec.Lit(true.B, false.B, false.B, false.B))
+      c.clock.step()
+      c.clock.step()
+      c.clock.step()
+      // Value should be held when enable is low.
+      c.io.out.expect(Vec.Lit(true.B, false.B, false.B, false.B))
+
+      // Step the MISR a few times
+      c.io.en.poke(true.B)
+      c.io.out.expect(Vec.Lit(true.B, false.B, false.B, false.B))
+      c.clock.step()
+      c.io.out.expect(Vec.Lit(true.B, true.B, true.B, false.B))
+      c.io.in.poke(Vec.Lit(false.B, false.B, true.B, true.B))
+      c.io.out.expect(Vec.Lit(true.B, true.B, true.B, false.B))
+      c.clock.step()
+      c.io.out.expect(Vec.Lit(true.B, true.B, false.B, false.B))
+      c.io.in.poke(Vec.Lit(true.B, true.B, true.B, false.B))
+      c.io.out.expect(Vec.Lit(true.B, true.B, false.B, false.B))
+      c.clock.step()
+      c.io.out.expect(Vec.Lit(true.B, false.B, false.B, false.B))
+      c.io.in.poke(Vec.Lit(false.B, false.B, false.B, true.B))
+      c.io.out.expect(Vec.Lit(true.B, false.B, false.B, false.B))
+      c.clock.step()
+      c.io.out.expect(Vec.Lit(false.B, true.B, false.B, true.B))
+    }
+  }
 }
 
