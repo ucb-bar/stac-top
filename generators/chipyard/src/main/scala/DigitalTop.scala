64f8b4698fe1eab6d990e6330383a74e91846524
rohanku <rohankumar@berkeley.edu>
2023-05-31 16:59:35 -0700

    undo changes to DigitalTop

--- generators/chipyard/src/main/scala/DigitalTop.scala
+++ generators/chipyard/src/main/scala/DigitalTop.scala
@@ -47,6 +47,5 @@ class DigitalTopModule[+L <: DigitalTop](l: L) extends ChipyardSystemModule(l)
   with sifive.blocks.devices.spi.HasPeripherySPIFlashModuleImp
   with sifive.blocks.devices.spi.HasPeripherySPIModuleImp
   with chipyard.example.CanHavePeripheryGCDModuleImp
-  with srambist.CanHavePeripherySramBistModuleImp
   with freechips.rocketchip.util.DontTouch
 // DOC include end: DigitalTop
