1f987eb81b3c36d35bc27f3d858dbd84228b4e3c
Rahul Kumar <rahulkumar@berkeley.edu>
2023-06-03 21:30:38 -0700

    Begin workin on scan-in bist test

--- generators/sram-bist/src/test/scala/SramBistSpec.scala
+++ generators/sram-bist/src/test/scala/SramBistSpec.scala
@@ -14,70 +14,72 @@ import srambist.programmablebist.ProgrammableBistParams
 import srambist.SramBistCtrlRegs._
 
 class SramBistTestHelpers(val c: SramBist) {
-  // val maxRows = 15;
-  // val maxCols = 7;
-  // val readOp = chiselTypeOf(
-  //   c.bist.io.elementSequence(0).operationElement.operations(0)
-  // ).Lit(
-  //   _.operationType -> c.bist.OperationType.read,
-  //   _.randData -> false.B,
-  //   _.randMask -> false.B,
-  //   _.dataPatternIdx -> 3.U,
-  //   _.maskPatternIdx -> 0.U,
-  //   _.flipped -> c.bist.FlipType.unflipped
-  // )
-  // val writeOp = chiselTypeOf(
-  //   c.bist.io.elementSequence(0).operationElement.operations(0)
-  // ).Lit(
-  //   _.operationType -> c.bist.OperationType.write,
-  //   _.randData -> false.B,
-  //   _.randMask -> false.B,
-  //   _.dataPatternIdx -> 3.U,
-  //   _.maskPatternIdx -> 0.U,
-  //   _.flipped -> c.bist.FlipType.unflipped
-  // )
-  // val readFlippedOp = chiselTypeOf(
-  //   c.bist.io.elementSequence(0).operationElement.operations(0)
-  // ).Lit(
-  //   _.operationType -> c.bist.OperationType.read,
-  //   _.randData -> false.B,
-  //   _.randMask -> false.B,
-  //   _.dataPatternIdx -> 3.U,
-  //   _.maskPatternIdx -> 0.U,
-  //   _.flipped -> c.bist.FlipType.flipped
-  // )
-  // val writeFlippedOp = chiselTypeOf(
-  //   c.bist.io.elementSequence(0).operationElement.operations(0)
-  // ).Lit(
-  //   _.operationType -> c.bist.OperationType.write,
-  //   _.randData -> false.B,
-  //   _.randMask -> false.B,
-  //   _.dataPatternIdx -> 3.U,
-  //   _.maskPatternIdx -> 0.U,
-  //   _.flipped -> c.bist.FlipType.flipped
-  // )
-  // val opElementList = Vec(4, new c.bist.Operation()).Lit(
-  //   0 -> writeOp,
-  //   1 -> readOp,
-  //   2 -> writeFlippedOp,
-  //   3 -> readFlippedOp
-  // )
-  // val opElement =
-  //   chiselTypeOf(c.bist.io.elementSequence(0).operationElement).Lit(
-  //     _.operations -> opElementList,
-  //     _.maxIdx -> 3.U,
-  //     _.dir -> c.bist.Direction.up,
-  //     _.numAddrs -> 0.U
-  //   )
-  // val waitElement = chiselTypeOf(c.bist.io.elementSequence(0).waitElement)
-  //   .Lit(_.cyclesToWait -> 0.U)
-  // val march = chiselTypeOf(c.bist.io.elementSequence(0)).Lit(
-  //   _.operationElement -> opElement,
-  //   _.waitElement -> waitElement,
-  //   _.elementType -> c.bist.ElementType.rwOp
-  // )
-  // val zeros = 0.U(32.W)
-  // val ones = "hffffffff".U(32.W)
+val maxRows = 15;
+val maxCols = 7;
+val readOp = chiselTypeOf(
+  c.bist.io.elementSequence(0).operationElement.operations(0)
+).Lit(
+  _.operationType -> c.bist.OperationType.read,
+  _.randData -> false.B,
+  _.randMask -> false.B,
+  _.dataPatternIdx -> 3.U,
+  _.maskPatternIdx -> 0.U,
+  _.flipped -> c.bist.FlipType.unflipped
+)
+val writeOp = chiselTypeOf(
+  c.bist.io.elementSequence(0).operationElement.operations(0)
+).Lit(
+  _.operationType -> c.bist.OperationType.write,
+  _.randData -> false.B,
+  _.randMask -> false.B,
+  _.dataPatternIdx -> 3.U,
+  _.maskPatternIdx -> 0.U,
+  _.flipped -> c.bist.FlipType.unflipped
+)
+val readFlippedOp = chiselTypeOf(
+  c.bist.io.elementSequence(0).operationElement.operations(0)
+).Lit(
+  _.operationType -> c.bist.OperationType.read,
+  _.randData -> false.B,
+  _.randMask -> false.B,
+  _.dataPatternIdx -> 3.U,
+  _.maskPatternIdx -> 0.U,
+  _.flipped -> c.bist.FlipType.flipped
+)
+val writeFlippedOp = chiselTypeOf(
+  c.bist.io.elementSequence(0).operationElement.operations(0)
+).Lit(
+  _.operationType -> c.bist.OperationType.write,
+  _.randData -> false.B,
+  _.randMask -> false.B,
+  _.dataPatternIdx -> 3.U,
+  _.maskPatternIdx -> 0.U,
+  _.flipped -> c.bist.FlipType.flipped
+)
+val opElementList = Vec(4, new c.bist.Operation()).Lit(
+  0 -> writeOp,
+  1 -> readOp,
+  2 -> writeFlippedOp,
+  3 -> readFlippedOp
+)
+val opElement =
+  chiselTypeOf(c.bist.io.elementSequence(0).operationElement).Lit(
+    _.operations -> opElementList,
+    _.maxIdx -> 3.U,
+    _.dir -> c.bist.Direction.up,
+    _.numAddrs -> 0.U
+  )
+val waitElement = chiselTypeOf(c.bist.io.elementSequence(0).waitElement)
+  .Lit(_.cyclesToWait -> 0.U)
+val march = chiselTypeOf(c.bist.io.elementSequence(0)).Lit(
+  _.operationElement -> opElement,
+  _.waitElement -> waitElement,
+  _.elementType -> c.bist.ElementType.rwOp
+)
+val elementSequence = Vec(4, new d.Element())
+          .Lit(0 -> march, 1 -> march, 2 -> march, 3 -> march)
+val zeros = 0.U(32.W)
+val ones = "hffffffff".U(32.W)
 
   // def populateSramRegisters(
   //     addr: UInt,
@@ -192,7 +194,7 @@ class SramBistSpec extends AnyFlatSpec with ChiselScalatestTester {
       )
     ) { c => }
   }
-  it should "work with chiseltest SRAMs" in {
+  it should "work with Write-Write-Read on chiseltest SRAMs" in {
     test(
       new SramBist(
       )(
@@ -287,6 +289,71 @@ class SramBistSpec extends AnyFlatSpec with ChiselScalatestTester {
       scanOut(SCAN_OUT_OFFSET(DOUT))
       scanOutAndAssert(REG_WIDTH(DOUT), 20)
       d.io.top.sramScanEn.poke(false.B)
+    }
+  }
+
+  it should "work with march BIST on chiseltest SRAMs" in {
+    test(
+      new SramBist(
+      )(
+        new WithChiseltestSrams(ChiseltestSramFailureMode.none)
+      )
+    ).withAnnotations(Seq(VcsBackendAnnotation, WriteVcdAnnotation)) { d =>
+      val scanIn = (width: Int, value: Int) => {
+        d.io.top.sramScanEn.poke(true.B)
+        var bitSeq = Seq[Int]()
+        var v = value
+        for (i <- 1 to width) {
+          bitSeq = (v % 2) +: bitSeq
+          v /= 2
+        }
+        for (bit <- bitSeq) {
+          d.io.top.sramScanIn.poke((bit == 1).B)
+          d.clock.step()
+        }
+        d.io.top.sramScanEn.poke(false.B)
+      }
+
+      val scanOutAndAssert = (width: Int, value: Int) => {
+        d.io.top.sramScanEn.poke(true.B)
+        var num = 0
+        for (i <- 1 to width) {
+          var bit = d.io.top.sramScanOut.peek().litToBoolean
+          var digit = if (bit) 1 else 0
+          num = num * 2 + digit
+          d.clock.step()
+        }
+        assert(num == value)
+        d.io.top.sramScanEn.poke(false.B)
+      }
+      val scanOut = (width: Int) => {
+        d.io.top.sramScanEn.poke(true.B)
+        for (i <- 1 to width) {
+          d.clock.step()
+        }
+        d.io.top.sramScanEn.poke(false.B)
+      }
+
+      val scanClear = () => {
+        d.io.top.sramScanIn.poke(false.B)
+        d.io.top.sramScanEn.poke(true.B)
+        for (i <- 1 to (TOTAL_REG_WIDTH + 2)) {
+          d.clock.step()
+        }
+        d.io.top.sramScanEn.poke(false.B)
+      }
+
+      d.clock.setTimeout(0)
+      val h = SramBistTestHelpers(d)
+      println(h.elementSequence.asBools)
+
+      d.io.top.sramExtEn.poke(true.B)
+      d.io.top.sramScanMode.poke(true.B)
+      d.io.top.sramEn.poke(false.B)
+      d.io.top.bistEn.poke(false.B)
+      d.io.top.bistStart.poke(false.B)
+
+      scanClear()
     }
   }
 }
