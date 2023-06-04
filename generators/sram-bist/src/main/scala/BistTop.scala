d8611afc59de4e515575498d325f79254cf17643
Rahul Kumar <rahulkumar@berkeley.edu>
2023-06-03 21:03:06 -0700

    Work on top level scan-chain tests (#14)
    * Work on top level scan-chain tests
    
    * Add manual test mode test for
    
    ---------
    
    Co-authored-by: Rahul Kumar <rahulkumar@eecs.berkeley.edu>

--- generators/sram-bist/src/main/scala/BistTop.scala
+++ generators/sram-bist/src/main/scala/BistTop.scala
@@ -16,7 +16,7 @@ case class BistTopParams(
 )
 
 object SramSrc extends ChiselEnum {
-  val bist, mmio = Value
+  val mmio, bist = Value
 }
 
 class BistTop(params: BistTopParams)(implicit p: Parameters) extends Module {
